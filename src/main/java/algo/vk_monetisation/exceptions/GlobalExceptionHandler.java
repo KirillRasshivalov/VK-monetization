e3f3e7ab2cfbce9148f530b53c95ab9eae4bf3e0
Kirill <kirill.rasshivalov@gmail.com>
2026-03-24 23:18:52 +0300

    Написал глобальный обработчик ошибок.

---
+++ src/main/java/algo/vk_monetisation/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package algo.vk_monetisation.exceptions;
+
+import algo.vk_monetisation.dto.ErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleAllExceptions(
+            Exception ex, WebRequest request) {
+        log.error("Произошла ошибка ", ex);
+        ErrorResponseDTO error = ErrorResponseDTO.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message("Непредвиденная ошибка.")
+                .path(request.getDescription(false))
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
