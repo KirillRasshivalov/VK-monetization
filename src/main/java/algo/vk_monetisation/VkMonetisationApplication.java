ed46b6a69f50a511dcae5bdc59569fb5ba021494
Kirill <kirill.rasshivalov@gmail.com>
2026-03-24 14:03:13 +0300

    initial commit.

---
+++ src/main/java/algo/vk_monetisation/VkMonetisationApplication.java
@@ -0,0 +1,13 @@
+package algo.vk_monetisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkMonetisationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkMonetisationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/algo/vk_monetisation/VkMonetisationApplicationTests.java
@@ -0,0 +1,13 @@
+package algo.vk_monetisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkMonetisationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
