ae05a22ba1f6b14b7094f3fec152825a94e24760
Kirill <kirill.rasshivalov@gmail.com>
2026-03-24 23:44:59 +0300

    Убрал лишние аннотации.

--- src/main/java/algo/vk_monetisation/dto/ErrorResponseDTO.java
+++ src/main/java/algo/vk_monetisation/dto/ErrorResponseDTO.java
@@ -1,13 +1,8 @@
 package algo.vk_monetisation.dto;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @Builder
 public record ErrorResponseDTO(
         int status,
--- src/main/java/algo/vk_monetisation/dto/LegalEntityDTO.java
+++ src/main/java/algo/vk_monetisation/dto/LegalEntityDTO.java
@@ -1,9 +1,6 @@
 package algo.vk_monetisation.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
 public record LegalEntityDTO(int index,
                              String region,
                              String town,
