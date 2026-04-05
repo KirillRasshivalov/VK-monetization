589a6b1bba45ffd01f33b64e86f230334f1bad78
Kirill <kirill.rasshivalov@gmail.com>
2026-04-05 08:57:05 +0300

    Закомментил автора, он у нас нигде не юзается.

--- src/main/java/algo/vk_monetisation/entities/Author.java
+++ src/main/java/algo/vk_monetisation/entities/Author.java
@@ -1,19 +1,19 @@
-package algo.vk_monetisation.entities;
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Table(name = "author")
-@Data
-public class Author {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    String nickname;
-
-    int followers_count;
-
-
-}
+//package algo.vk_monetisation.entities;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Entity
+//@Table(name = "author")
+//@Data
+//public class Author {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    String nickname;
+//
+//    int followers_count;
+//
+//
+//}
